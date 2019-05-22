0d668cdf0b6040a3554e1940ddb5ec4a9a49b5d0
Alice Gamarnik <agamarnik1@pride.hofstra.edu>
2019-05-21 23:59:23 -0400

    Inital Commit

---
+++ Snake/GameManager.swift
@@ -0,0 +1,9 @@
+//
+//  GameManager.swift
+//  GameManager
+//
+//  Created by Alice Gamarnik on 5/21/19.
+//  Copyright © 2019 Alice Gamarnik. All rights reserved.
+//
+
+import Foundation
---
+++ Snake/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Snake
+//
+//  Created by Alice Gamarnik on 5/21/19.
+//  Copyright © 2019 Alice Gamarnik. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
