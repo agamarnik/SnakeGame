315a3a8c4ccf4f4ab6700bd256d8f79984c155be
Alice Gamarnik <agamarnik1@pride.hofstra.edu>
2019-05-22 00:42:48 -0400

    Loading Game View and created game instance

--- Snake/GameManager.swift
+++ Snake/GameManager.swift
@@ -1,9 +1,43 @@
 //
 //  GameManager.swift
-//  GameManager
+//  Snake
 //
 //  Created by Alice Gamarnik on 5/21/19.
 //  Copyright © 2019 Alice Gamarnik. All rights reserved.
 //
 
 import Foundation
+import SpriteKit
+
+class GameManager {
+    var scene: GameScene!
+    
+    init(scene: GameScene) {
+        self.scene = scene
+    }
+    
+    //1
+    func initGame() {
+        //starting player position
+        scene.playerPositions.append((10, 10))
+        scene.playerPositions.append((10, 11))
+        scene.playerPositions.append((10, 12))
+        renderChange()
+    }
+    //2
+    func renderChange() {
+        for (node, x, y) in scene.gameArray {
+            if contains(a: scene.playerPositions, v: (x,y)) {
+                node.fillColor = SKColor.cyan
+            } else {
+                node.fillColor = SKColor.clear
+            }
+        }
+    }
+    //3
+    func contains(a:[(Int, Int)], v:(Int,Int)) -> Bool {
+        let (c1, c2) = v
+        for (v1, v2) in a { if v1 == c1 && v2 == c2 { return true } }
+        return false
+    }
+}
